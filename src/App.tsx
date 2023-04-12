8fa4955a8ebdccad891f0da1b09287ec9617c849
katy <katy.feng.14@gmail.com>
2023-04-12 15:47:08 -0400

    basic request setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import {CreateUserForm} from "./Components/CreateUserForm";
+import {LoanTable} from "./Components/LoanTable";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    setLoading(true);
+    const url = `https://cors-anywhere.herokuapp.com/https://lending-api.azurewebsites.net/users`;
+    axios.get(url, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Content-Type': 'application/json',
+      }
+    })
+        .then((response) => {
+          setData(response.data);
+          setLoading(false);
+        })
+        .catch((e) => console.log(e))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {data && data.map((user: any) => (
+            <div>{user.username}</div>
+        ))}
+
+        <CreateUserForm />
+        <LoanTable userId={1} />
+      </div>
   );
 }
 
